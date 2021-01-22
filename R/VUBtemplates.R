bbdc7d1c3e099a53e4e1229c27f3c5fa33e6ea96
ElizaVdS <eliza.vds@hotmail.com>
2021-01-22 22:51:22 +0100

    commit

--- R/VUBtemplates.R
+++ R/VUBtemplates.R
@@ -12,7 +12,7 @@ NULL
 #  Largely inspired from the rticles package
 #' @keywords internal
 find_file <- function (template, file) {
-  template <- system.file("rmarkdown", "templates", template, file, package = "VUBtemplates")
+  template <- system.file("rmarkdown", "templates", template, file, package = "VUBtemplate")
   if (template == "") {
     stop("Couldn't find template file ", template, "/", file, call. = FALSE)
   }
@@ -42,3 +42,62 @@ inherit_pdf_document <- function (...) {
 article <- function (..., md_extensions = c("-autolink_bare_uris")) {
   inherit_pdf_document(..., template = find_resource("article", "template.tex"), md_extensions = md_extensions, citation_package = "natbib")
 }
+
+#' Knit
+#'
+#' Creation of all the documents from the models
+#'
+#' Used to verify that the models function well
+#'
+#' @param destination Destionation file of the documents
+#'
+#' @name Knit
+NULL
+
+
+#' @rdname Knit
+#' @export
+KnitAll <- function (destination="docs") {
+  KnitArticle(destination=destination)
+  TricoterPresentation(destination=destination)
+  TricoterOuvrage(destination=destination)
+  TricoterMemo(destination=destination)
+}
+
+#' @rdname Knit
+#' @export
+KnitArticle <- function (destination="docs") {
+  # Preparation
+  knitr_table_format <- options("knitr.table.format")
+  OriginalWD <- getwd()
+  tmpdir <- tempdir()
+  # Article
+  setwd(tmpdir)
+  unlink("article", recursive=TRUE)
+  rmarkdown::draft("article", template="article", package="VUBtemplate", edit=FALSE)
+  setwd("article")
+  # Knit to HTML
+  options(knitr.table.format='html')
+  rmarkdown::render(input="article.Rmd",
+                    output_format=bookdown::html_document2(theme="sandstone", toc=TRUE, toc_float=TRUE),
+                    output_dir=destination)
+  # Knit to pdf
+  options(knitr.table.format='latex')
+  rmarkdown::render(input="article.Rmd",
+                    output_format=bookdown::pdf_book(base_format=EcoFoG::article),
+                    output_dir=destination)
+  # Copy to destination
+  docsDirs <- list.dirs(path=destination, full.names=TRUE, recursive=TRUE)
+  dir.create(paste(OriginalWD, "/", destination, sep=""), showWarnings=FALSE)
+  dir.create(paste(OriginalWD, "/", destination, "/article", sep=""), showWarnings=FALSE)
+  if (length(docsDirs) > 0) {
+    sapply(paste(OriginalWD, "/", destination, "/article/", docsDirs, sep=""), dir.create, showWarnings=FALSE)
+    docsFiles <- list.files(destination, full.names=TRUE, recursive=TRUE)
+    file.copy(from=docsFiles, to=paste(OriginalWD, "/", destination, "/article/", docsFiles, sep=""), overwrite=TRUE)
+  }
+  # Clean up
+  setwd(OriginalWD)
+  unlink(paste(tmpdir, "/article", sep=""), recursive=TRUE)
+  options(knitr.table.format=knitr_table_format)
+}
+
